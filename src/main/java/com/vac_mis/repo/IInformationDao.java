be2d876bf381591d0028df0f680e546384a44f5d
BugingoBlaise <blaisemugisha1@gmail.com>
2023-12-02 16:37:02 +0200

    some query fixes on service interfaces

--- src/main/java/com/vac_mis/repo/IInformationDao.java
+++ src/main/java/com/vac_mis/repo/IInformationDao.java
@@ -3,11 +3,14 @@ package com.vac_mis.repo;
 import com.vac_mis.model.Information;
 import com.vac_mis.model.Vaccine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+
 @Repository
 public interface IInformationDao extends JpaRepository<Information, UUID> {
+    @Query("SELECT i FROM Information i WHERE i.patientName=?1")
     Information findInformationByName(String name);
 
 }
--- src/main/java/com/vac_mis/repo/IVaccineDao.java
+++ src/main/java/com/vac_mis/repo/IVaccineDao.java
@@ -2,11 +2,14 @@ package com.vac_mis.repo;
 
 import com.vac_mis.model.Vaccine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+
 @Repository
 public interface IVaccineDao extends JpaRepository<Vaccine, UUID> {
+    @Query("SELECT v FROM Vaccine v WHERE v.name=?1")
     Vaccine findVaccineByName(String name);
 
 }
