20213f2baa253d2325c18c79b4f1dad686905aa9
BugingoBlaise <blaisemugisha1@gmail.com>
2023-12-07 00:07:56 +0200

    on security issues

--- src/main/java/com/vac_mis/controller/UsersController.java
+++ src/main/java/com/vac_mis/controller/UsersController.java
@@ -5,6 +5,7 @@ import com.vac_mis.model.enums.ERole;
 import com.vac_mis.service.IUsersService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,23 +18,25 @@ public class UsersController {
     private final IUsersService userService;
 
     @Autowired
-    public UsersController(IUsersService userService) {
+    public UsersController(IUsersService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
+
+    private final PasswordEncoder passwordEncoder;
 
 
     @GetMapping("/login")
     public String homePage(Model model) {
-        model.addAttribute("log",new Users());
+        model.addAttribute("log", new Users());
         return "login";
     }
-
-
 
 
     @PostMapping("/login/createAccount")
     public String signup(@ModelAttribute("log") Users user) {
         // Implement your user registration logic here (e.g., save user details in the database)
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         user.setRole(ERole.NURSE);
 
---
+++ src/main/java/com/vac_mis/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.vac_mis.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.context.DelegatingApplicationListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfiguration{
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+}
